bdeeec712a73054d194fa4b15e65181b7a107ee9
shalomc <shalom@venera.com>
2022-11-10 10:57:35 +0200

    first code commit

---
+++ day-1/ToggleButton/ToggleButton.ino
@@ -0,0 +1,30 @@
+const int ledPin = 13;          // choose the pin for the LED
+const int inputPin = 2;         // choose the input pin (for a pushbutton)
+const int speakerPin = 9;
+int button_value = 0;                    // variable for reading the pin status
+boolean is_turned_on = false;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT_PULLUP);     // declare pushbutton as input
+}
+
+void loop() {
+  button_value = digitalRead(inputPin);  // read input value
+  delay(5); // allow button to stabilize
+  // We initialized the input pin with INPUT_PULLUP
+  // therefore LOW means pressed, HIGH means not pressed.
+  if (button_value == LOW ) {            // check if the input is HIGH
+    is_turned_on = !is_turned_on; // flip button
+    digitalWrite(ledPin, is_turned_on);
+    // loop while button pressed
+    while (digitalRead(inputPin) == LOW) {
+      delay(1);
+    }
+  }
+  else {
+    ;
+    // This is a placeholder for things to do when the button is NOT pressed
+  }
+
+}
---
+++ day-1/ToggleButton2/ToggleButton2.ino
@@ -0,0 +1,39 @@
+const int ledPin = 13;          // choose the pin for the LED
+const int inputPin = 2;         // choose the input pin (for a pushbutton)
+const int speakerPin = 9;
+int button_value = 0;                    // variable for reading the pin status
+boolean is_turned_on = false;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT_PULLUP);     // declare pushbutton as input
+}
+
+void loop() {
+  button_value = digitalRead(inputPin);  // read input value
+  delay(5); // allow button to stabilize
+  // We initialized the input pin with INPUT_PULLUP
+  // therefore LOW means pressed, HIGH means not pressed.
+  if (button_value == LOW ) {            // check if the input is HIGH
+    if (is_turned_on) { // flip button
+      is_turned_on = false ;
+    } else {
+      is_turned_on = true ;
+    }
+    if (is_turned_on) {
+      digitalWrite(ledPin, HIGH);  // turn LED ON
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
+
+    // loop while button pressed
+    while (digitalRead(inputPin) == LOW) {
+      delay(1);
+    }
+  }
+  else {
+    ;
+    // This is a placeholder for things to do when the button is NOT pressed
+  }
+
+}
