4e9c4eb87b193cdc8f6458365e24e2cb9e8b1bd1
shalomc <shalom@venera.com>
2022-11-24 15:58:00 +0200

    adjust tabbing

--- day-3/ServoDrummer/ServoDrummer.ino
+++ day-3/ServoDrummer/ServoDrummer.ino
@@ -8,13 +8,13 @@ Servo servo;
 
 int initialAngle = 90; // servo position in degrees
 
-// Beats 
-int full = 600; 
-int half = full/2; 
-int quarter = half/2; 
-int eighth = quarter/2; 
+// Beats
+int full = 600;
+int half = full / 2;
+int quarter = half / 2;
+int eighth = quarter / 2;
 
-int tuneFlipper = 1; 
+int tuneFlipper = 1;
 
 void setup() {
   servo.attach(servoPin);
@@ -23,73 +23,71 @@ void setup() {
 
 
 void loop() {
-	switch (tuneShuffle) {
-		case 1: 
-			WilemTel();
-			break;
-		case 2: 
-			Bolero();
-			break;
-		default: 
-			tuneFlipper=0; 
-			break; 
-	}	
-	tuneFlipper++; 
-	delay(40);
+  switch (tuneShuffle) {
+    case 1:
+      WilemTel();
+      break;
+    case 2:
+      Bolero();
+      break;
+    default:
+      tuneFlipper = 0;
+      break;
+  }
+  tuneFlipper++;
+  delay(40);
 
 }
 
 void WilemTel() {
-	int beats[MAX_SCORE_SIZE] = { 
-		eighth, eighth, quarter, 
-		eighth, eighth, quarter, 
-		eighth, eighth, quarter,  quarter,  quarter, 
-		eighth, eighth, quarter, 
-		eighth, eighth, quarter, 
-		eighth, eighth, quarter,  quarter,  quarter ,
-		eighth, eighth, quarter, 
-		eighth, eighth, quarter, 
-		eighth, eighth, quarter,  quarter,  quarter, 
-		eighth, eighth, half, 
-		eighth, eighth, eighth, quarter, quarter, quarter
-	};
-	playScore(beats); 
- }
+  int beats[MAX_SCORE_SIZE] = {
+    eighth, eighth, quarter,
+    eighth, eighth, quarter,
+    eighth, eighth, quarter,  quarter,  quarter,
+    eighth, eighth, quarter,
+    eighth, eighth, quarter,
+    eighth, eighth, quarter,  quarter,  quarter ,
+    eighth, eighth, quarter,
+    eighth, eighth, quarter,
+    eighth, eighth, quarter,  quarter,  quarter,
+    eighth, eighth, half,
+    eighth, eighth, eighth, quarter, quarter, quarter
+  };
+  playScore(beats);
+}
 
 
 void Bolero() {
-	int beats[MAX_SCORE_SIZE] = { 
-		full, quarter, quarter, quarter,
-		full, quarter, quarter, quarter,
-		full, full, 
-		full, quarter, quarter, quarter,
-		full, quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,quarter
-	};
-	for (int i=0 ; i<2; i++) {
-		playScore(beats); 
-	}
- }
+  int beats[MAX_SCORE_SIZE] = {
+    full, quarter, quarter, quarter,
+    full, quarter, quarter, quarter,
+    full, full,
+    full, quarter, quarter, quarter,
+    full, quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter
+  };
+  for (int i = 0 ; i < 2; i++) {
+    playScore(beats);
+  }
+}
 
 void doBeat() {
   int delayVal = 60;
   servo.write(160); //down
   delay(delayVal);
   servo.write(100); //up
-//  delay(delayVal);
+  //  delay(delayVal);
 }
 
 
 
 
 void playScore(int (&notesArray)[MAX_SCORE_SIZE] ) {
-	// (sizeof(notesArray)/sizeof(int))-1 
-	//will always be the number of items in the array
-	for (int note=0 ; note < (sizeof(notesArray)/sizeof(int)) - 1 ; note++) {
-		if (notesArray[note]>0) {
-			doBeat(); 
-			delay(notesArray[note]); 
-		}
-	}
+  // (sizeof(notesArray)/sizeof(int))-1
+  //will always be the number of items in the array
+  for (int note = 0 ; note < (sizeof(notesArray) / sizeof(int)) - 1 ; note++) {
+    if (notesArray[note] > 0) {
+      doBeat();
+      delay(notesArray[note]);
+    }
+  }
 }
-
-
