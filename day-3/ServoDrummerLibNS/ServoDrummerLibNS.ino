f00937dc5852d4565fc00dc6e925eee248d40be6
shalomc <shalom@venera.com>
2022-11-30 16:06:37 +0200

    Update ServoDrummerLibNS.ino

--- day-3/ServoDrummerLibNS/ServoDrummerLibNS.ino
+++ day-3/ServoDrummerLibNS/ServoDrummerLibNS.ino
@@ -5,9 +5,11 @@
 
 // parameters for the robot engines
 // parameters for the drumming servo
-#define SERVO_PIN_1  3
+#define SERVO_PIN_1  13
 #define DIAGNOSTIC_MODE false
 
+int tuneShuffle = 1;
+int beats01[DRUM_MAX_SCORE_SIZE] = {200, 300, 200, 300, 100, 100, 100, 500, 500, 500};
 
 
 void setup() {
@@ -16,17 +18,30 @@ void setup() {
 }
 
 void loop() {
-  Drum::Bolero();
-  int beats[DRUM_MAX_SCORE_SIZE] = {200, 300, 200, 300, 100, 100, 100, 500, 500, 500};
-  for (int i = 0; i < 10; i++) {
-    Drum::doBeat();
-    delay(300);
+  switch (tuneShuffle) {
+    case 1:
+      Drum::WilemTel();
+      tuneShuffle++;
+      break;
+    case 2:
+      Drum::Bolero();
+      tuneShuffle++;
+      break;
+    case 3:
+      Drum::playScore(beats01);
+      tuneShuffle++;
+      break;
+    case 4:
+      for (int i = 0; i < 10; i++) {
+        Drum::doBeat();
+        delay(Drum::half);
+      }
+      tuneShuffle++;
+      break;
+    default:
+      tuneShuffle = 1;
+      break;
   }
+  delay(400);
 
-  for (int i = 0; i < 10; i++) {
-    Drum::playScore(beats);
-    delay(300);
-  }
-
-  delay(10);
 }
